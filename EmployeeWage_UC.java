044ddcceeb5ba539a215e67cdd70d6be0f0eda24
shubhampandey111 <shubhampandey051197@gmail.com>
2020-11-10 10:55:13 +0530

    Employee_WC_UC

---
+++ EmployeeWage_UC.java
@@ -0,0 +1,15 @@
+/**
+ *
+ * @author Dell
+ */
+public class EmployeeWage_UC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Employee wage Computation Program ");
+    }
+    
+}
